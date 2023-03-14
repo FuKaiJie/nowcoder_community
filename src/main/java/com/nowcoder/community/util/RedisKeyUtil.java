812224341aa21a33a875d463103c33185f02dc8d
FuKaiJie <1617568777@qq.com>
2023-03-14 17:25:29 +0800

    收到的赞

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -1,5 +1,7 @@
 package com.nowcoder.community.util;
 
+import javax.lang.model.element.NestingKind;
+
 /**
  * @author FKJ
  * @create 2023-03-13 20:30
@@ -8,6 +10,7 @@ public class RedisKeyUtil {
 
     private static final String SPLIT = ":";
     private static final String PREFIX_ENEITY_LIKE = "like:entity";
+    private static final String PREFIX_USER_LIKE = "like:user";
 
     //某个实体的赞
     //like:entity:entityType:entityId -> set(userId)
@@ -15,4 +18,9 @@ public class RedisKeyUtil {
         return PREFIX_ENEITY_LIKE + SPLIT + entityType + SPLIT + entityId;
     }
 
+    //某个用户的赞
+    //like:user:userId -> int
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
 }
